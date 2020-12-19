4df6fd1e2a572bb495d4be0fca77aa5f5d197085
sphan35 <seanphan5@gmail.com>
2020-12-18 19:46:26 -0600

    UFOs JavaScript Webpage Challenge Complete.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // from data.js
 const tableData = data;
-
+// console.log(data);
 // get table references
 var tbody = d3.select("tbody");
 
@@ -37,18 +37,18 @@ function updateFilters() {
     let elementValue = changedElement.property("value");
     console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterId = changedElemt.attr("id");
+    let filterId = changedElement.attr("id");
     console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
 
     if (elementValue) {
-      filters[filterID] = elementValue; 
+      filters[filterId] = elementValue; 
     }
 
     else {
-      delete filters[filterID];
+      delete filters[filterId];
     }
   
     // 6. Call function to apply all filters and rebuild the table
@@ -57,23 +57,27 @@ function updateFilters() {
   }
   
   // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
+function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    let filteredData = tableData;
+  let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    if (changedElement) {
-      filteredData = filteredData.filter(row => row.this === changedElement);
-    }
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+        
+  });
+
+
   
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredData);
-  }
+  buildTable(filteredData);
+
+}
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("input").on("change", updateFilters);
+d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
-  buildTable(tableData);
+buildTable(tableData);
